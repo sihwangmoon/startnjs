107991f25fb88c224b82504b27d4c925468d9b72
sihwang <dante.cclss@gmail.com>
2017-04-18 22:02:46 +0900

    version0.0.1

--- myboard/routes/index.js
+++ myboard/routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Board'});
+});
+
+router.post('/signup', passport.authenticate('signup', {
+    successRedirect: '/profile',
+    failureRedirect: '/signup',
+    failureFlash: true
+}))
+router.post('/login', passport.authenticate('login', {
+    successRedirect: '/profile',
+    failureRedirect: '/login',
+    failureFlash: true
+}))
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()){
+        return next();
+    } else {
+        res.redirect('/login');
+    }
+}
+
+router.get('/profile', isLoggedIn, function(req, res, next) {
+    res.render('index', { title: 'You are logged in.' });
 });
 
 module.exports = router;
